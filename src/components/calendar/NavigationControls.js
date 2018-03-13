deb4572c34698ad417f8bba16f11e75220726901
EC2 Default User <ec2-user@ip-172-31-42-197.us-east-2.compute.internal>
2018-03-13 22:06:26 +0000

    working on navigation

--- src/components/calendar/NavigationControls.js
+++ src/components/calendar/NavigationControls.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
+import FontAwesome from 'react-fontawesome';
 
 const Wrapper = styled.div.attrs({})``;
 
+const LeftNav = styled.span.attrs({
+    className:"chevron-circle-left"
+})``;
 
 class NavigationControls extends Component{
 
     
     render(){
         return(
-            <Wrapper>Hello World</Wrapper>    
+            <Wrapper>
+            <div>
+                <FontAwesome name='chevron-circle-left' />
+            </div>
+           </Wrapper>    
         );
     }
     
