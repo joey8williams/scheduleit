5a59575c3df2578ceda77ebf5d7cb61d1ae41a0e
joey8williams <joey8williams@gmail.com>
2018-03-21 20:51:27 +0000

    syncing branch

--- src/theme/elements/Button.js
+++ src/theme/elements/Button.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components';
 
 const Button = styled.button.attrs({
-    className:props => props.primary ? 'b--light-red light-red f4 fw5 ba br3 bw1' 
-                     : props.default ? 'b--mid-gray mid-gray f4 fw5 ba br3 bw1' 
-                                     : 'b--moon-gray moon-gray f4 fw5 ba br3 bw1'
+    className:props => props.primary ? 'b--light-red light-red f5 fw5 ba br3 bw1' 
+                     : props.default ? 'b--mid-gray mid-gray f5 fw5 ba br3 bw1' 
+                                     : 'b--moon-gray moon-gray f5 fw5 ba br3 bw1'
 })`
     min-height:${props => props.heightSpec};
-    
-
 `;
 
 export {Button};
